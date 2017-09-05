40464555887ad5eebfb1c392775bb7a31f66213b
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-09-05 23:44:46 +0200

    Refactor tests

--- test/test.js
+++ test/test.js
@@ -7,6 +7,8 @@ var createTempFile = require('../')
 require('string.prototype.startswith')
 require('string.prototype.endswith')
 
+const noop = function () {}
+
 function ctf (ext) {
   var ws = createTempFile(ext)
   ws.on('error', function (err) {
@@ -18,14 +20,18 @@ function ctf (ext) {
 test('write stream works', function (t) {
   t.plan(2)
   var ws = ctf()
-  fs.createReadStream(__dirname + '/do-not-change.txt')
+  fs
+    .createReadStream(path.join(__dirname, '/do-not-change.txt'))
     .pipe(ws)
     .on('finish', testWS)
 
   function testWS () {
-    fs.readFile(ws.path, { encoding: 'utf8' }, function (err, string) {
+    fs.readFile(ws.path, {encoding: 'utf8'}, function (err, string) {
       t.notOk(err, err ? err.message : 'no error')
-      t.ok(string.startsWith('do not delete or change this file'), 'strings match')
+      t.ok(
+        string.startsWith('do not delete or change this file'),
+        'strings match'
+      )
       ws.cleanupSync()
       t.end()
     })
@@ -50,6 +56,25 @@ test('extension', function (t) {
   t.end()
 })
 
+function errorHandlingSync (opts, method, codes) {
+  return function errhandle (t) {
+    var ws = createTempFile(opts)
+
+    ws.on('error', function (e) {
+      t.notEqual(codes.indexOf(e.code), -1, 'Got ' + codes.join('/') + ' error')
+      t.end()
+    })
+    setTimeout(function () {
+      try {
+        fs.unlinkSync(ws.path)
+      } catch (e) {
+        t.fail(String(e))
+      }
+      setTimeout(ws[method], 10)
+    }, 10)
+  }
+}
+
 function errorHandling (opts, method, codes) {
   return function errhandle (t) {
     var ws = createTempFile(opts)
@@ -62,19 +87,31 @@ function errorHandling (opts, method, codes) {
       try {
         fs.unlinkSync(ws.path)
       } catch (e) {
-        console.log('here')
         t.fail(String(e))
       }
-      setTimeout(ws[method], 10)
+      setTimeout(ws[method], 10, noop)
     }, 10)
   }
 }
 
-test('error handling sync default params', errorHandling({}, 'cleanupSync', ['EPERM', 'ENOENT']))
-test('error handling sync explicit enoent', errorHandling({enoent: true}, 'cleanupSync', ['EPERM', 'ENOENT']))
+test(
+  'error handling sync default params',
+  errorHandlingSync({}, 'cleanupSync', ['EPERM', 'ENOENT'])
+)
+test(
+  'error handling sync explicit enoent',
+  errorHandlingSync({enoent: true}, 'cleanupSync', ['EPERM', 'ENOENT'])
+)
 
-test('error handling async default params', errorHandling({}, 'cleanup', ['EPERM', 'ENOENT']))
-test('error handling async explicit enoent', errorHandling({enoent: true}, 'cleanup', ['EPERM', 'ENOENT']))
+test(
+  'error handling callback async default params',
+  errorHandling({}, 'cleanup', ['EPERM', 'ENOENT'])
+)
+
+test(
+  'error handling callback async explicit enoent',
+  errorHandling({enoent: true}, 'cleanup', ['EPERM', 'ENOENT'])
+)
 
 test('cleanupSync() works', function (t) {
   t.plan(3)
@@ -82,7 +119,8 @@ test('cleanupSync() works', function (t) {
   var ws = ctf()
   ws.end('lolz')
 
-  setTimeout(function () { // timeout makes this test much more robust
+  setTimeout(function () {
+    // timeout makes this test much more robust
     t.ok(fs.existsSync(ws.path), 'created file')
     t.doesNotThrow(ws.cleanupSync)
     t.notOk(fs.existsSync(ws.path), 'cleanupSync() deleted the file')
@@ -96,7 +134,8 @@ test('cleanup() works', function (t) {
   var ws = ctf()
   ws.end('lolz')
 
-  setTimeout(function () { // timeout makes this test much more robust
+  setTimeout(function () {
+    // timeout makes this test much more robust
     t.ok(fs.existsSync(ws.path), 'created file')
     ws.cleanup(function (err) {
       t.notOk(err, err ? err.message : 'no error during cleanup()')
@@ -106,7 +145,7 @@ test('cleanup() works', function (t) {
   }, 0)
 })
 
-function callsEnd (method) {
+function callsEndSync (method) {
   return function (t) {
     t.plan(1)
     var ws = ctf()
@@ -119,6 +158,26 @@ function callsEnd (method) {
   }
 }
 
-callsEnd('what')
-test('ws.end is called when cleanup is called and stream is still going', callsEnd('cleanup'))
-test('ws.end is called when cleanupSync is called and stream is still going', callsEnd('cleanupSync'))
+function callsEnd (method) {
+  return function (t) {
+    t.plan(1)
+    var ws = ctf()
+    ws.on('finish', function () {
+      t.pass('ws.end() was called')
+      t.end()
+    })
+    ws.write('lolz')
+    setTimeout(ws[method], 0, noop)
+  }
+}
+
+callsEndSync('what')
+
+test(
+  'ws.end is called when cleanup is called and stream is still going',
+  callsEnd('cleanup')
+)
+test(
+  'ws.end is called when cleanupSync is called and stream is still going',
+  callsEndSync('cleanupSync')
+)
